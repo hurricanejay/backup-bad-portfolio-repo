aaaa9680988c54bb615c13d0bdcd5e3c4d156a87
hurricanejay <jaywen89@gmail.com>
2020-03-28 16:17:48 -0400

    created react portfolio

---
+++ build/precache-manifest.19cf5e20a540eb440d8b6f685c69a141.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c982709378873836cd43dae113451ae",
+    "url": "/hurricanejay.github.io/index.html"
+  },
+  {
+    "revision": "5ad756039682bba46de2",
+    "url": "/hurricanejay.github.io/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "bad7d0a21fdcd82f6a6d",
+    "url": "/hurricanejay.github.io/static/css/main.16787939.chunk.css"
+  },
+  {
+    "revision": "5ad756039682bba46de2",
+    "url": "/hurricanejay.github.io/static/js/2.0a4014ad.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/hurricanejay.github.io/static/js/2.0a4014ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bad7d0a21fdcd82f6a6d",
+    "url": "/hurricanejay.github.io/static/js/main.6c296228.chunk.js"
+  },
+  {
+    "revision": "9a6fbec9575ff002a701",
+    "url": "/hurricanejay.github.io/static/js/runtime-main.1e1e7354.js"
+  },
+  {
+    "revision": "9f33ec70f386277b7b79e9c93565c8c0",
+    "url": "/hurricanejay.github.io/static/media/heirloom_salad.9f33ec70.png"
+  },
+  {
+    "revision": "b068a4217ac641f254cbf02ee9cb2edd",
+    "url": "/hurricanejay.github.io/static/media/ice_cream.b068a421.jpg"
+  },
+  {
+    "revision": "f3684128ee25289b04546c86e7c7c43e",
+    "url": "/hurricanejay.github.io/static/media/townhouse.f3684128.JPG"
+  }
+]);
