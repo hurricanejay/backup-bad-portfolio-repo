c23a82e3f6aa340d26b7dd057da6530cf7e0f6a6
hurricanejay <jaywen89@gmail.com>
2020-04-22 10:16:14 -0400

    Updates

---
+++ precache-manifest.c3c245f0ab76a2d0eee518484fa38d97.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20e72006bc80c69a5f82d835c272ac0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a2ea40d2e8a107fb988",
+    "url": "/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "2c54d7f8e8e4639e094f",
+    "url": "/static/css/main.08efb7b9.chunk.css"
+  },
+  {
+    "revision": "0a2ea40d2e8a107fb988",
+    "url": "/static/js/2.f5b069d8.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.f5b069d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c54d7f8e8e4639e094f",
+    "url": "/static/js/main.92aa08a5.chunk.js"
+  },
+  {
+    "revision": "a27870e5f03a3425f6aa",
+    "url": "/static/js/runtime-main.c70048c8.js"
+  },
+  {
+    "revision": "188f382062c37c87b7aaabd695717745",
+    "url": "/static/media/goodspace.188f3820.png"
+  },
+  {
+    "revision": "5a1f57c65ef585ca8b4ee97c8766afb4",
+    "url": "/static/media/jayheadshot.5a1f57c6.jpg"
+  },
+  {
+    "revision": "2c79adb76fe30f205404aaa2de811215",
+    "url": "/static/media/plantify.2c79adb7.png"
+  }
+]);
